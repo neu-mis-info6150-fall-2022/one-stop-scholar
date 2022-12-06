fce763725db12ca4c15a0382eab405404fc4c731
Riya-Rajesh-Patil <patil.riy@northeastern.edu>
2022-12-05 20:06:15 -0500

    testing git

--- backend/api/app.js
+++ backend/api/app.js
@@ -3,19 +3,11 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import routes from './routes/index.js';
 
-// 'express.json' To instruct the application that the incoming HTTP requests contains JSON payload
-// 'express.urlencoded' To instruct the application that the incoming HTTP requests contains URL encoded payload
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
-// Post request is recieved it will be forwarded to router to decode URL
 routes(app);
-
-// Connect to mongodb using mongoose connect function
-// export const nextAuthDb = mongoose.createConnection('mongodb+srv://onestopscholar:OneStopScholarINFO6150@onestopscholar.cpgxpwf.mongodb.net/nextauthDb?retryWrites=true&w=majority');
-// mongoose.connect('mongodb+srv://onestopscholar:OneStopScholarINFO6150@onestopscholar.cpgxpwf.mongodb.net/?retryWrites=true&w=majority');
-
 
 export default app;
