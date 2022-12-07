4e72703dc3946af79046137597699cbb0dd73874
ritviksaxena05 <saxena.rit@northeastern.edu>
2022-12-07 10:59:37 -0500

    Adding scss to scholarship page

--- frontend/pages/scholarship/index.js
+++ frontend/pages/scholarship/index.js
@@ -38,7 +38,7 @@ function scholarshipList({ scholarships }) {
                     <Link href={`/scholarship/${scholarship._id}`} legacyBehavior>  
                       <a className={styles.schName}>{scholarship.scholarshipName}</a>
                     </Link>
-                    <p className={styles.schAmt}>${scholarship.scholarshipAmt}</p>
+                    <p className={styles.schAmt}>Upto ${scholarship.scholarshipAmt}</p>
                     <p className={styles.details}>Desc: {scholarship.scholarshipDescription}</p>
                     <p className={styles.details}>Sponser: {scholarship.scholarshipSponsor}</p>
                     <p className={styles.details}>Date Posted: {date}</p>
